2470ee360f1d9dd8696878f8ae95e1e1b8587b15
Minh Dang <minhdang150601@gmail.com>
2023-11-21 00:53:13 +0700

    feat: implement api get all user

---
+++ BACKEND/data.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE se15_2 CHARACTER SET utf8 COLLATE utf8_general_ci;
+INSERT INTO `se15.2`.`user`
+(id, salary, sex, username)
+VALUES(1, '100', 'MALE', 'Đặng Đức Minh');
+INSERT INTO `se15.2`.`user`
+(id, salary, sex, username)
+VALUES(2, '150', 'FEMALE', 'Nguyễn Thị Vân');
